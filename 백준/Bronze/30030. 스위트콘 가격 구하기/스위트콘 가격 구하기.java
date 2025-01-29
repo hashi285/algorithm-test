94a3fcbc54e14b53af85f72e044d65539f862fd8
suhyeok moon <155638514+hashi285@users.noreply.github.com>
2025-01-29 23:05:07 +0900

    [Bronze V] Title: 스위트콘 가격 구하기, Time: 120 ms, Memory: 15884 KB -BaekjoonHub

---
+++ 백준/Bronze/30030. 스위트콘 가격 구하기/스위트콘 가격 구하기.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int A = Integer.parseInt(br.readLine());
+
+        bw.write((A - A/11) + "\n");
+        bw.flush();
+        bw.close();
+    }
+}
