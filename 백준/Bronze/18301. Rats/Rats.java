ed3ed168c9d5a06016fb50658e5f0ef2fdcf5c02
suhyeok moon <155638514+hashi285@users.noreply.github.com>
2025-02-19 20:24:59 +0900

    [Bronze V] Title: Rats, Time: 116 ms, Memory: 15740 KB -BaekjoonHub

---
+++ 백준/Bronze/18301. Rats/Rats.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        bw.write((A + 1) * (B + 1) / (C + 1) - 1 + "\n");
+        bw.flush();
+        bw.close();
+
+
+    }
+}
