e9497af6e6864c9bf54fd7eb2a4cc3ad17e7c735
suhyeok moon <155638514+hashi285@users.noreply.github.com>
2025-02-20 15:25:35 +0900

    [Bronze V] Title: Correct, Time: 132 ms, Memory: 15864 KB -BaekjoonHub

---
+++ 백준/Bronze/26307. Correct/Correct.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        bw.write(60 * (A - 9) + B + "\n");
+        bw.flush();
+        bw.close();
+    }
+}
