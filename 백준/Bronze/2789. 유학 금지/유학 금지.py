993fb23cd04959c33228a9e1a8b2fa46fdcb64c2
suhyeok moon <m51187251@gmail.com>
2026-01-16 09:58:52 +0900

    [Bronze II] Title: 유학 금지, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2789. 유학 금지/유학 금지.py
@@ -0,0 +1,13 @@
+T = input()
+
+T = T.replace('C','')
+T = T.replace('A','')
+T = T.replace('M','')
+T = T.replace('B','')
+T = T.replace('R','')
+T = T.replace('I','')
+T = T.replace('D','')
+T = T.replace('G','')
+T = T.replace('E','')
+
+print(T)
